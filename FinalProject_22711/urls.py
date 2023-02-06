c67ad6f1258f81b60e12cf7f87ea64fab96eb1ff
Graciele89 <gracieleludwig@yahoo.com.br>
2023-02-06 23:08:23 +0000

    templates and dynamic view context

--- FinalProject_22711/urls.py
+++ FinalProject_22711/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.views.generic.base import TemplateView
 
 from carpool import urls as carpool_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(carpool_urls, namespace='carpool')),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- carpool/views.py
+++ carpool/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    # https://ccbv.co.uk/projects/Django/4.1/django.views.generic.base/TemplateView/
+    def get_context_data(self, **kwargs):
+     context= super().get_context_data(**kwargs)
+     context['my_object_whatsoever'] = "Hello: this is dynamic"
+     return context
